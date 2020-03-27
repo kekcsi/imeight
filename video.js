0b109300ce92f64ed2c1a5e5c00a0581decbcd21
tchibo <tchibo@82420f3a-7163-4616-a83c-df5120c3470c>
2020-03-27 18:21:02 +0000

    Tutor formatting

---
+++ video.js
@@ -0,0 +1,25 @@
+builtInArrays.SPRX = [] //horizontal coordinate of each sprite (-23 to 384)
+builtInArrays.SPRY = [] //vertical coordinate of each sprite (-23 to 216)
+builtInArrays.SPRLIGHT = [] //intensity mask pointers (0*144 to 255*144)
+builtInArrays.SPRRED = [] //red mask pointers (0*144 to 255*144)
+builtInArrays.SPRGREEN = [] //green mask pointers (0*144 to 255*144)
+builtInArrays.SPRBLUE = [] //blue mask pointers (0*144 to 255*144)
+builtInArrays.SPRPRIO = [] //priorities
+
+builtInArrays.TEXT = [] //64+2 by 48+2 bytes
+
+builtInArrays.TILEI = [] //16+2 by 9+2 pointers (0*144 to 255*144)
+builtInArrays.TILER = [] //16+2 by 9+2 pointers (0*144 to 255*144)
+builtInArrays.TILEG = [] //16+2 by 9+2 pointers (0*144 to 255*144)
+builtInArrays.TILEB = [] //16+2 by 9+2 pointers (0*144 to 255*144)
+
+builtInVariables.BACKGROUND = %1000 //IRGB nibble
+builtInVariables.DESIGNS = 0 //mask pointer offset
+
+builtInVariables.TILEX = 0 //-24 to 23
+builtInVariables.TILEY = 0 //-24 to 23
+
+builtInVariables.TEXTX = 0 //-8 to 7
+builtInVariables.TEXTY = 0 //-8 to 7
+builtInVariables.TEXTFILL = 0 //0 or 1
+
