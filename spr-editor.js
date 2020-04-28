3a6ebf4fb27f465596d1b4b7ab40a9769d396913
tchibo <tchibo@82420f3a-7163-4616-a83c-df5120c3470c>
2020-04-28 23:08:24 +0000

    Font editor bugfixes, transforms

--- spr-editor.js
+++ spr-editor.js
@@ -1,7 +1,8 @@
+var selSlot = 0
+
 var designData = []
 var drawingColor = "#000000"
 var drawingIdx = 0
-var selDesign = 0
 
 var designModes = []
 var designModeMap = []
@@ -22,8 +23,11 @@ setInterval(function() {
 	if (designerDirty) {
 		if (window.navigator.userAgent.indexOf("Edge/") < 0 || window.location.protocol != "file:") {
 			localStorage.setItem("designdata", JSON.stringify(designData))
-			localStorage.setItem("seldesign", selDesign)
+			localStorage.setItem("fontdata", JSON.stringify(fontData))
+			localStorage.setItem("selslot", selSlot)
+			localStorage.setItem("selglyph", selGlyph)
 			localStorage.setItem("memory", JSON.stringify(memory))
+			localStorage.setItem("designmodemap", JSON.stringify(designModeMap))
 		}
 
 		designerDirty = false
@@ -35,26 +39,28 @@ setInterval(function() {
 	}
 	
 	cells = tblMemory.getElementsByTagName("td")	
-	if (cells[selDesign].style.background != "white") {
-		cells[selDesign].style.background = "white"
+	if (cells[selSlot].style.background != "white") {
+		cells[selSlot].style.background = "white"
 	} else {
-		cells[selDesign].style.background = "gray"
+		cells[selSlot].style.background = "gray"
 	}
 }, 2000)
 
 function changeDesignMode() {
-	designModeMap[selDesign]++
-	designModeMap[selDesign]%=designModes.length
+	designModeMap[selSlot]++
+	designModeMap[selSlot]%=designModes.length
 	updateDesignMode()
+	
+	designFromMemory()
 }
 
 function updateDesignMode() {
-	var m = designModeMap[selDesign]
+	var m = designModeMap[selSlot]
 	for (var i in designModes) {
 		designModes[i].tab.style.display = ((m == i)?"block":"none")
 	}
 	
-	fontFromMemory()
+	setDrawingIndex(7)
 }
 
 function updateFont() {
@@ -68,10 +74,15 @@ function updateFont() {
 
 function goFont(glyphIdx) {
 	if (cbGetDesign.checked) {
-		fontToMemory(selDesign, selGlyph)
+		fontToMemory(selSlot, selGlyph)
 	}
+
+	var anchs = divGlyph.getElementsByTagName("a")
+	anchs[selGlyph].style.backgroundColor = "gray"
 	
 	selGlyph = isNaN(glyphIdx) ? glyphIdx.charCodeAt(0) - 55 : glyphIdx
+
+	anchs[selGlyph].style.backgroundColor = "white"
 	
 	if (cbGetDesign.checked) {
 		fontFromMemory()
@@ -115,9 +126,15 @@ function clearDesigner() {
 	designData = []
 
 	var pixCells = tblDesigner.getElementsByTagName("td")
-	for(var i = 0; i < pixCells.length; ++i) {
-		designData.push(drawingIdx)
+	for (var i = 0; i < pixCells.length; ++i) {
+		designData[i] = drawingIdx
 		pixCells[i].style.backgroundColor = drawingColor
+	}
+	
+	var bitCells = tblDesignerGlyph.getElementsByTagName("td")
+	for (var j = 0; j < bitCells.length; ++j) {
+		fontData[j] = 0
+		bitCells[j].style.backgroundColor = "black"
 	}
 }
 
@@ -130,8 +147,8 @@ function clearDesignModes() {
 }
 
 pageLoadHooks.push(function() {
-	designModes = [{ tab: tabSprTile, put: sprToMemory, get: sprFromMemory }, 
-			{ tab: tabFont, put: fontToMemory, get: fontFromMemory }]
+	designModes = [{ tab: tabSprTile, put: sprToMemory, get: sprFromMemory, data: designData, thumb: sprThumb }, 
+			{ tab: tabFont, put: fontToMemory, get: fontFromMemory, data: fontData, thumb: fontThumb }]
 	
 	clearDesigner()
 	clearDesignModes()
@@ -143,14 +160,22 @@ pageLoadHooks.push(function() {
 			updateDesign()
 		}
 
-		selDesign = localStorage.getItem("seldesign")
-		if (selDesign == null) selDesign = 0
-		inPointer.value = 288*selDesign					
+		var fontDataJson = localStorage.getItem("fontdata")
+		if (fontDataJson) {
+			fontData = JSON.parse(fontDataJson)
+			updateFont()
+		}
+
+		selSlot = localStorage.getItem("selslot")
+		if (selSlot == null) selSlot = 0
+		inPointer.value = 288*selSlot					
+
+		selGlyph = localStorage.getItem("selglyph")
+		if (selGlyph == null) selGlyph = 0
 		
 		designModeMapJson = localStorage.getItem("designmodemap")
 		if (designModeMapJson) {
 			designModeMap = JSON.parse(designModeMapJson)
-			updateDesignMode()
 		}
 
 		var memoryJson = localStorage.getItem("memory")
@@ -158,6 +183,9 @@ pageLoadHooks.push(function() {
 			memory = JSON.parse(memoryJson)
 		}
 	}
+
+	var anchs = divGlyph.getElementsByTagName("a")
+	anchs[selGlyph].style.backgroundColor = "white"
 
 	document.body.addEventListener("mousedown", function(ev) {
 		mouseButtons = 1
@@ -190,7 +218,7 @@ pageLoadHooks.push(function() {
 		})
 	}
 	
-	setDrawingIndex(7)
+	updateDesignMode()
 	
 	cells = tblMemory.getElementsByTagName("td")
 	for(var j = 0; j < 227; ++j) {
@@ -201,17 +229,19 @@ pageLoadHooks.push(function() {
 		}
 		
 		cell.addEventListener("mousedown", function(ev) {
-			var prevSel = selDesign
-			selDesign = parseInt(ev.target.dataset.idx, 10)
-			updateDesignMode()
+			var prevSel = selSlot
+			selSlot = parseInt(ev.target.dataset.idx, 10)
 			
-			inPointer.value = 288*selDesign					
+			inPointer.value = 288*selSlot					
 			cells[prevSel].style.background = "black"
-			cells[selDesign].style.background = "white"
+			cells[selSlot].style.background = "white"
 
 			if (cbGetDesign.checked) {
 				designToMemory(prevSel)
+				updateDesignMode()
 				designFromMemory()
+			} else {
+				designModeMap[selSlot] = designModeMap[prevSel]
 			}
 		})
 	}
@@ -245,11 +275,16 @@ pageLoadHooks.push(function() {
 
 function designToMemory(designIdx) {
 	if (designIdx === undefined) {
-		designIdx = selDesign
+		designIdx = selSlot
 	}
 	
 	var putFn = designModes[designModeMap[designIdx]].put
 	putFn(designIdx)
+	
+	updateThumb(designIdx)
+	memoryUpdateHook(288*designIdx)
+	updateDownloadBlob()
+	designerDirty = true
 }
 
 function sprToMemory(designIdx) {
@@ -257,12 +292,6 @@ function sprToMemory(designIdx) {
 		var octet = ((designData[2*i]<<4)|designData[2*i + 1])
 		memory[i + 288*designIdx] = octet
 	}
-	
-	updateThumb(designIdx)
-	memoryUpdateHook(288*designIdx)
-	updateDownloadBlob()
-	
-	designerDirty = true
 }
 
 function fontToMemory(designIdx, glyph) {
@@ -282,13 +311,13 @@ function fontToMemory(designIdx, glyph) {
 }
 
 function designFromMemory() {
-	var getFn = designModes[designModeMap[selDesign]].get
+	var getFn = designModes[designModeMap[selSlot]].get
 	getFn()
 }
 
 function sprFromMemory() {
 	for (var i = 0; i < 288; ++i) {
-		octet = memory[i + 288*selDesign]
+		octet = memory[i + 288*selSlot]
 		if (!octet) octet = 0
 		designData[2*i] = (octet >> 4)
 		designData[2*i + 1] = (octet & 15)
@@ -299,7 +328,7 @@ function sprFromMemory() {
 
 function fontFromMemory() {
 	for (var ro = 0; ro < 8; ro++) {
-		var by = memory[selDesign*288 + selGlyph*8 + ro]
+		var by = memory[selSlot*288 + selGlyph*8 + ro]
 
 		for (var co = 0; co < 8; co++) {
 			fontData[8*ro + co] = ((by>>(7 - co))&1)
@@ -324,19 +353,7 @@ function updateThumb(designIndex) {
 	
 	if (designIndex > 226) return false
 	
-	for (var ro = 0; ro < 16; ++ro) {
-		for (var co = 0; co < 16; ++co) {
-			var i = 12*Math.floor(ro*3/2) + Math.floor(co*3/4)
-			
-			octet = memory[i + 288*designIndex]
-			if (!octet) octet = 0
-			var bytes = ((co%2) ? colorToBytes(octet&15) : colorToBytes(octet>>4))
-
-			for (var k = 0; k < 4; ++k) {
-				thumb[4*co + 64*ro + k] = bytes[k]
-			}
-		}
-	}
+	genThumb(designIndex, thumb)
 
 	imd = new ImageData(thumb, 16, 16)
 	var canvas = document.createElement('canvas')
@@ -351,34 +368,86 @@ function updateThumb(designIndex) {
 	return true
 }
 
+function genThumb(designIndex, thumb) {
+	designModes[designModeMap[designIndex]].thumb(designIndex, thumb)
+}
+
+function sprThumb(designIndex, thumb) {
+	for (var ro = 0; ro < 16; ++ro) {
+		for (var co = 0; co < 16; ++co) {
+			var i = 12*Math.floor(ro*3/2) + Math.floor(co*3/4)
+			
+			octet = memory[i + 288*designIndex]
+			if (!octet) octet = 0
+			var bytes = ((co%2) ? colorToBytes(octet&15) : colorToBytes(octet>>4))
+
+			for (var k = 0; k < 4; ++k) {
+				thumb[4*co + 64*ro + k] = bytes[k]
+			}
+		}
+	}
+}
+
+function fontThumb(designIndex, thumb) {
+	var glyphs = [0, 1, 10, 11]
+	var xoffs = [0, 8, 0, 8]
+	var yoffs = [0, 0, 8, 8]
+
+	for (var gi in glyphs) {
+		var glyph = glyphs[gi]
+		var xoff = xoffs[gi]
+		var yoff = yoffs[gi]
+		
+		for (var ro = 0; ro < 8; ro++) {
+			var by = memory[designIndex*288 + glyph*8 + ro]
+
+			for (var co = 0; co < 8; co++) {
+				var bit = ((by>>(7 - co))&1)
+
+				var tup = [(bit ? 255 : 0), 0, 0, (bit ? 255 : 0)]
+				
+				for (var k = 0; k < 4; k++) {
+					thumb[k + 4*(16*(ro + yoff) + co + xoff)] = tup[k]
+				}
+			}
+		}
+	}
+}
+
 function transformDesign(hFlip, vFlip, transpose, dx, dy) {
 	var transformed = []
 	
-	for (var ox = 0; ox < 24; ox++) {
-		for (var oy = 0; oy < 24; oy++) {
+	var data = designModes[designModeMap[selSlot]].data
+	var sz = Math.sqrt(data.length)
+	
+	for (var ox = 0; ox < sz; ox++) {
+		for (var oy = 0; oy < sz; oy++) {
 			if (transpose) {
-				var x = (oy + dy + 24)%24
-				var y = (ox + dx + 24)%24
+				var x = (oy + dy + sz)%sz
+				var y = (ox + dx + sz)%sz
 			} else {
-				var x = (ox + dx + 24)%24
-				var y = (oy + dy + 24)%24
+				var x = (ox + dx + sz)%sz
+				var y = (oy + dy + sz)%sz
 			}
 			
 			if (hFlip) {
-				x = 23 - x
+				x = sz - x - 1
 			}
 			
 			if (vFlip) {
-				y = 23 - y
+				y = sz - y - 1
 			}
 			
-			transformed[x + 24*y] = designData[ox + 24*oy]
+			transformed[x + sz*y] = data[ox + sz*oy]
 		}
 	}
 	
-	designData = transformed
+	for (var i = 0; i < data.length; i++) {
+		data[i] = transformed[i]
+	}
 	
 	updateDesign()
+	updateFont()
 }
 
 function swapColor(color) {
